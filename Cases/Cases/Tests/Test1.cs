dfc16e7e3aab56d03d471a065484b911761d63c3
5toon <vitalii.vyniarskyi@gmail.com>
2020-04-06 14:01:37 +0300

    empy project

---
+++ Cases/Cases/Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Cases.Tests
+{
+    public class Test1
+    {
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine("Testing");
+        }
+    }
+}
