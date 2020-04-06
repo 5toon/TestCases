7fad800ac46e56152873cd6838f8438bcafeee9e
5toon <vitalii.vyniarskyi@gmail.com>
2020-04-06 15:12:15 +0300

    first step

--- Cases/Cases/Tests/Test1.cs
+++ Cases/Cases/Tests/Test1.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace Cases.Tests
 {
     public class Test1
     {
+        private readonly IWebDriver _driver;
+        private static string url = "http://automationpractice.com/index.php";
+
+        public Test1()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            _driver.Navigate().GoToUrl(url);
+        }
+
         [Test]
         public void Test()
         {
